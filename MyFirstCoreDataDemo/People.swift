cd041c1d4e81af73fa893ed80f402e026caa9e75
xinnyu <panxinyu@xinyudeMacBook-Pro.local>
2015-08-24 13:25:27 +0800

    完成

---
+++ MyFirstCoreDataDemo/People.swift
@@ -0,0 +1,16 @@
+//
+//  People.swift
+//  MyFirstCoreDataDemo
+//
+//  Created by 潘新宇 on 15/8/23.
+//  Copyright (c) 2015年 潘新宇. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class People: NSManagedObject {
+
+    @NSManaged var name: String
+
+}
